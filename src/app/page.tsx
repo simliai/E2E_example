da1d7d9e167faa45106494ead307d0d8bac59ba9
Waleed <waleed.nizamaniwn@gmail.com>
2024-12-03 10:37:27 +0100

    added prompt and other fields

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,12 @@ export default function Home() {
   const [tempRoomUrl, setTempRoomUrl] = useState<string>("");
   const [callObject, setCallObject] = useState<DailyCall | null>(null);
   const myCallObjRef = useRef<DailyCall | null>(null);
-  
   const [chatbotId, setChatbotId] = useState<string | null>(null);
-  
+  const [apiKey, setApiKey] = useState<string>("");
+  const [faceId, setFaceId] = useState<string>("");
+  const [voiceId, setVoiceId] = useState<string>("");
+  const [prompt, setPrompt] = useState<string>("");
+
   const loadChatbot = async () => {
     if (myCallObjRef.current) {
 
@@ -49,48 +52,86 @@ export default function Home() {
     }
   }
 
+  const handleJoinRoom = async () => {
+    let roomUrl = tempRoomUrl.toString();
+    if (roomUrl === "") {
+      if (apiKey.trim() === "" || faceId.trim() === "" || voiceId.trim() === "" || prompt.trim() === "") {
+        console.log("API Key, Face ID, Voice ID, and Prompt are required");
+        window.alert("API Key, Face ID, Voice ID, and Prompt are required");
+        return;
+      }
+      // Call the API to get the room URL
+      const response = await fetch("https://api.simli.ai/startE2ESession", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          apiKey: apiKey.trim(),
+          faceId: faceId.trim(),
+          voiceId: voiceId.trim(),
+          prompt: prompt.trim(),
+        }),
+      })
+
+      const data = await response.json();
+      roomUrl = data.roomUrl;
+    }
+    let newCallObject = DailyIframe.getCallInstance();
+
+    if (newCallObject === undefined) {
+      newCallObject = DailyIframe.createCallObject({
+        videoSource: false,
+      });
+    }
+
+    newCallObject.setUserName("My name");
+
+    await newCallObject.join({ url: roomUrl });
+    myCallObjRef.current = newCallObject;
+    console.log("Joined the room with callObject", newCallObject);
+    setCallObject(newCallObject);
+
+    setTimeout(loadChatbot, 2000);
+
+
+  }
 
   return (
-    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center justify-between p-10  h-screen">
       {
         callObject && (
-          <DailyProvider callObject={callObject}>
-            {
-              chatbotId && (
-                <Tile key={chatbotId} id={chatbotId} />
+          <div className="h-96 w-96 ">
+            <DailyProvider callObject={callObject}>
+              {
+                chatbotId && (
+                  <Tile key={chatbotId} id={chatbotId} />
 
-              )
-            }
-          </DailyProvider>
+                )
+              }
+            </DailyProvider>
+          </div>
         )
       }
 
-      <input type="text" className="bg-slate-100 px-2 py-1 rounded-md" placeholder="Room URL" value={tempRoomUrl} onChange={(e) => setTempRoomUrl(e.target.value)} />
+      {
+        !callObject && (
+          <div className="flex flex-row gap-4 items-center">
+            <input type="text" className="bg-slate-100 px-2 py-1 rounded-md" placeholder="Room URL" value={tempRoomUrl} onChange={(e) => setTempRoomUrl(e.target.value)} />
+            <div className="font-bold">OR</div>
+            <div className="flex flex-col gap-2">
+              <input type="text" className="bg-slate-100 px-2 py-1 rounded-md" placeholder="API Key" value={apiKey} onChange={(e) => setApiKey(e.target.value)} />
+              <input type="text" className="bg-slate-100 px-2 py-1 rounded-md" placeholder="Face ID" value={faceId} onChange={(e) => setFaceId(e.target.value)} />
+              <input type="text" className="bg-slate-100 px-2 py-1 rounded-md" placeholder="Voice ID" value={voiceId} onChange={(e) => setVoiceId(e.target.value)} />
+              <textarea className="bg-slate-100 px-2 py-1 rounded-md" placeholder="Prompt" value={prompt} onChange={(e) => setPrompt(e.target.value)} />
+            </div>
+          </div>
+        )
+      }
 
       <div className="flex flex-row gap-2">
 
-        <button className="bg-green-500 text-white px-2 py-1 rounded-md" onClick={async () => {
-          console.log("Joining room", tempRoomUrl)
-
-          let newCallObject = DailyIframe.getCallInstance();
-
-          if (newCallObject === undefined) {
-            newCallObject = DailyIframe.createCallObject({
-              videoSource: false,
-            });
-          }
-
-          newCallObject.setUserName("My name");
-
-          await newCallObject.join({ url: tempRoomUrl });
-          myCallObjRef.current = newCallObject;
-          console.log("Joined the room with callObject", newCallObject);
-          setCallObject(newCallObject);
-
-          setTimeout(loadChatbot, 2000);
-
-
-        }}>Join room</button>
+        <button className="bg-green-500 text-white px-2 py-1 rounded-md" onClick={handleJoinRoom}>Join room</button>
         <button className="bg-red-500 text-white px-2 py-1 rounded-md" onClick={handleLeaveRoom}>Leave room</button>
         <button className="bg-slate-500 text-white px-2 py-1 rounded-md" onClick={handleMute}>Mute</button>
       </div>
